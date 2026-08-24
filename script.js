4805e9be0e834562c19a4af4ae8cf82d2ffe9de8
OmkarMensi <omkarmensi29@gmail.com>
2026-08-24 11:46:44 +0530

    First commit on main

---
+++ script.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+document.getElementById("counterBtn").addEventListener("click", () => {
+    count++;
+    document.getElementById("count").innerText = `Counter: ${count}`;
+});
+
+document.getElementById("colorBtn").addEventListener("click", () => {
+
+    const colors = [
+        "#FFCDD2",
+        "#C8E6C9",
+        "#BBDEFB",
+        "#FFF9C4",
+        "#E1BEE7"
+    ];
+
+    const random =
+        colors[Math.floor(Math.random()*colors.length)];
+
+    document.body.style.background = random;
+});
